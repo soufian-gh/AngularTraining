539d35001133b4e84dfb06eb902cd4e399aa3728
souf <01souf01.com>
2022-05-18 21:11:11 +0100

    initial

---
+++ cards/src/app/app.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+ // title = 'cards';
+ posts = [
+   {
+     title : "neat tree",
+     imageUrl : "assets/tree.jpeg",
+     userName : "nature",
+     content : "paragraph one"
+   },
+   {
+    title : "biking",
+    imageUrl : "assets/biking.jpeg",
+    userName : "biker",
+    content : "paragraph two"
+  },
+  {
+    title : "mountain",
+    imageUrl : "assets/mountain.jpeg",
+    userName : "mount",
+    content : "paragraph three"
+  },
+ ];
+}
--- pw/src/app/app.component.ts
+++ pw/src/app/app.component.ts
@@ -1,86 +1,58 @@
-import { BuiltinType } from '@angular/compiler';
 import { Component } from '@angular/core';
-
-const handler = (e: { target: HTMLInputElement }) => {
-  const value = e.target.value;
-}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-
-//event binding syntax     <button (click)="method()" ><button/>
-//property binding syntax  <input [value]=password> <input/>
-
-
 export class AppComponent {
-
-  password ='';
-
+  length = 0;
   includeLetters = false;
   includeNumbers = false;
   includeSymbols = false;
-  lenght = 6 ;
-   // Event handler for lenth
+  password = '';
 
-
-   onChangeLength(value:string){
-   
+  onChangeLength(value: string) {
     const parsedValue = parseInt(value);
-    if(!isNaN(parsedValue)) {
-      this.lenght=parsedValue;
+
+    if (!isNaN(parsedValue)) {
+      this.length = parsedValue;
     }
-  };
-    // Event handler for checkboxes
+  }
 
-  onChangeUseLetters(){
+  onChangeUseLetters() {
     this.includeLetters = !this.includeLetters;
-  };
-  
-  onChangeUseNumbers(){
+  }
+
+  onChangeUseNumbers() {
     this.includeNumbers = !this.includeNumbers;
-  };
+  }
 
-  onChangeUseSymbols(){
+  onChangeUseSymbols() {
     this.includeSymbols = !this.includeSymbols;
-  };
+  }
 
-  onButtonClick(){
-   // this.password='password test'
- 
-
+  onButtonClick() {
     const numbers = '1234567890';
-    const letters = 'AZERTYUIOOPQSDFGHJKLMWXCVBN';
-    const symbols = '@*$^%µ£#';
+    const letters = 'abcdefghijklmnopqrstuvwyz';
+    const symbols = '!@#$%^&*()';
 
     let validChars = '';
-
-    if(this.includeLetters){
+    if (this.includeLetters) {
       validChars += letters;
-    };
-
-    if(this.includeSymbols){
-      validChars += symbols;
-    };
-
-    if(this.includeNumbers){
+    }
+    if (this.includeNumbers) {
       validChars += numbers;
-    };
-
-    let generatedPass = '';
-    for (let i = 0; i < length; i++) {
-      const index = Math.floor(Math.random() * validChars.length);
-      generatedPass += validChars[index];
+    }
+    if (this.includeSymbols) {
+      validChars += symbols;
     }
 
-    this.password = generatedPass;
-
-
-    console.log(this.password);
-
-    
-  };
-  
+    let generatedPassword = '';
+    for (let i = 0; i < this.length; i++) {
+      const index = Math.floor(Math.random() * validChars.length);
+      generatedPassword += validChars[index];
+    }
+    this.password = generatedPassword;
+  }
 }
